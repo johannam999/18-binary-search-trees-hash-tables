990fd48048cf81d83d085805cb3b6a40c625ed0e
Panos Akio Kumasaka <panosrockbear@gmail.com>
2018-05-09 16:08:39 -0700

    initial work done

---
+++ src/lib/binary-search.js
@@ -0,0 +1,75 @@
+'use strict';
+
+class BinarySearchTree {
+  constructor(root = null) {
+    this.root = root;
+  }
+
+  insert(nodeToInsert) {
+    if (!this.root) {
+      this.root = nodeToInsert;
+    } else {
+      this._insert(this.root, nodeToInsert);
+    }
+  }
+
+  remove(value){
+    let nodeToRemove = this.find(value);
+    nodeToRemove = _remove(nodeToRemove);
+  }
+
+  __remove(node){
+    if(!nodeToRemove.left&&!nodeToRemove.right)
+    {return(null);}
+    
+    if(!nodeToRemove.right)
+    {
+        return(nodeToRemove.left);
+    }
+    if(nodeToRemove.left)
+    {
+        return(nodeToRemove.right);
+    }
+    else{
+
+    }
+  }
+
+  _insert(rootNode, nodeToInsert) {
+    if (nodeToInsert.value < rootNode.value) {
+      if (!rootNode.left) {
+        rootNode.left = nodeToInsert;
+      } else {
+        this._insert(rootNode.left, nodeToInsert);
+      }
+    } else if (!rootNode.right) {
+      
+      rootNode.right = nodeToInsert;
+    } else {
+      
+      this._insert(rootNode.right, nodeToInsert);
+    }
+  }
+
+
+  find(value) {
+    if (!this.root) {
+      return null;
+    }
+    return this._find(this.root, value);
+  }
+
+  _find(rootNode, value) {
+    if (!rootNode) {
+      return null;
+    } else if (rootNode.value === value) {
+      return rootNode;
+    } else if (rootNode.value < value) {
+      return this._find(rootNode.right, value);
+    }
+    return this._find(rootNode.left, value);
+  }
+  
+}
+
+export default BinarySearchTree;

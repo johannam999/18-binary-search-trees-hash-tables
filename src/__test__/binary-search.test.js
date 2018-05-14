1ca362ceec74cee65c7d2a14db2e779175232f21
Kris Sakarias <ksakarias@gmail.com>
2018-05-13 19:58:46 -0700

    fixing linter errors

--- src/__test__/binary-search.test.js
+++ src/__test__/binary-search.test.js
@@ -1,53 +1,53 @@
 'use strict';
 
-import binarySearchTree from '../lib/binary-search-tree';
-import binaryNode from '../lib/binary-node'; 
+import BinarySearchTree from '../lib/binary-search-tree';
+import BinaryNode from '../lib/binary-node'; 
 
-let testTree=new binarySearchTree();
+const testTree = new BinarySearchTree();
 
-let node1=new binaryNode(4);
-let node2=new binaryNode(2);
-let node3=new binaryNode(5);
-let node4=new binaryNode(6);
-let node5=new binaryNode(10);
-let node6=new binaryNode(1);
+const node1 = new BinaryNode(4);
+const node2 = new BinaryNode(2);
+const node3 = new BinaryNode(5);
+const node4 = new BinaryNode(6);
+const node5 = new BinaryNode(10);
+const node6 = new BinaryNode(1);
 
-describe('testing insertions',()=>{
-    test('Should change root node', () =>{
-        testTree.insert(node1);
-        expect(testTree.root.value).toEqual(4);
-    })
-    test('Should be on left',()=>{
-        testTree.insert(node2);
-        expect(testTree.root.left.value).toEqual(2);
-    })
-    test('Should be on right',()=>{
-        testTree.insert(node4)
-        expect(testTree.root.right.value).toEqual(6);
-    })
-    test('Should be left of right',()=>{
-        testTree.insert(node3);
-        expect(testTree.root.right.left.value).toEqual(5);
-    })
-    test('Should be right of right',()=>{
-        testTree.insert(node5);
-        expect(testTree.root.right.right.value).toEqual(10);
-    })
-    test('Should be left of left',()=>{
-        testTree.insert(node6);
-        expect(testTree.root.left.left.value).toEqual(1);
-    })
-})
+describe('testing insertions', () => {
+  test('Should change root node', () => {
+    testTree.insert(node1);
+    expect(testTree.root.value).toEqual(4);
+  });
+  test('Should be on left', () => {
+    testTree.insert(node2);
+    expect(testTree.root.left.value).toEqual(2);
+  });
+  test('Should be on right', () => {
+    testTree.insert(node4);
+    expect(testTree.root.right.value).toEqual(6);
+  });
+  test('Should be left of right', () => {
+    testTree.insert(node3);
+    expect(testTree.root.right.left.value).toEqual(5);
+  });
+  test('Should be right of right', () => {
+    testTree.insert(node5);
+    expect(testTree.root.right.right.value).toEqual(10);
+  });
+  test('Should be left of left', () => {
+    testTree.insert(node6);
+    expect(testTree.root.left.left.value).toEqual(1);
+  });
+});
 
-describe('testing find',()=>{
-    test('should return proper node', ()=>{
-        expect(testTree.find(4)).toEqual(node1);
-    })
-})
-describe('testing remove',()=>{
-    test('should replace left of root with its child',()=>{
-        testTree.remove(2);
-        console.log(testTree);
-        expect(testTree.root.left.value).toEqual(1);
-    })
-})
+describe('testing find', () => {
+  test('should return proper node', () => {
+    expect(testTree.find(4)).toEqual(node1);
+  });
+});
+describe('testing remove', () => {
+  test('should replace left of root with its child', () => {
+    testTree.remove(2);
+    console.log(testTree);
+    expect(testTree.root.left.value).toEqual(1);
+  });
+});
--- src/lib/binary-search-tree.js
+++ src/lib/binary-search-tree.js
@@ -5,75 +5,89 @@ class BinarySearchTree {
     this.root = root;
   }
 
-  remove(value){
-    let oldNode=this.root
-    let nodeToBeRemoved=this.findForRemove(value,oldNode)
+  remove(value) {
+    const oldNode = this.root;
+    const nodeToBeRemoved = this.findForRemove(value, oldNode);
     console.log(nodeToBeRemoved);
-    this.__remove(nodeToBeRemoved,oldNode);
+    this.__remove(nodeToBeRemoved, oldNode);
   }
 
-  __remove(rootNode,oldNode){
+  __remove(rootNode, oldNode) {
     console.log(rootNode);
     console.log(oldNode);
-    let position=false;
-    if(oldNode.left===rootNode)
-    {position=true;}
-    if(!rootNode.left&&!rootNode.right)
-    {if(position===false){oldNode.left=null;}
-      else{oldNode.right=null};
-  }
-  if(rootNode.left&&!rootNode.right){
-    if(position===true){oldNode.left=rootNode.left;}
-        else{oldNode.right=rootNode.left;}}
+    let position = false;
+    if (oldNode.left === rootNode) {
+      position = true; 
+    }
+    if (!rootNode.left && !rootNode.right) {
+      if (position === false) { 
+        oldNode.left = null; 
+      } else { 
+        oldNode.right = null; 
+      }
+    }
+    if (rootNode.left && !rootNode.right) {
+      if (position === true) { 
+        oldNode.left = rootNode.left; 
+      } else { 
+        oldNode.right = rootNode.left; 
+      } 
+    } else if (!rootNode.left && rootNode.right) {
+      if (position === true) { 
+        oldNode.left = rootNode.right; 
+      } else { 
+        oldNode.right = rootNode.left; 
+      }
+    }
 
-  else if (!rootNode.left&&rootNode.right){
-    if(position===true){oldNode.left=rootNode.right;}
-      else{oldNode.right=rootNode.left;}}
-
-  if(rootNode.left&&rootNode.right)
-  { let currentDifference=0;
-    let nodeToMove;
-    this.removeValueComparer(rootNode,rootNode.value)
-    if(left===true)
-      {oldNode.left=nodeToMove}
-    else
-      {oldNode.right=nodeToMove}}
+    if (rootNode.left && rootNode.right) {
+      const currentDifference = 0;
+      let nodeToMove;
+      this.removeValueComparer(rootNode, rootNode.value);
+      if (left === true) {
+        oldNode.left = nodeToMove;
+      } else {
+        oldNode.right = nodeToMove;
+      }
+    }
   }
-  findForRemove(value,oldNode) {
-    if (!this.root) {  
+
+  findForRemove(value, oldNode) {
+    if (!this.root) {
       return null;    
     }      
-    if(this.root.value===value)     
-    {return(this.root)}
-    return this._findForRemove(this.root,value,oldNode);              
+    if (this.root.value === value) {
+      return (this.root);
+    }
+    return this._findForRemove(this.root, value, oldNode);              
   }                
 
   _findForRemove(rootNode, value, oldNode) {
-    if (!rootNode) {                      
+    if (!rootNode) {                  
       return null;                        
-    }
-    else if (rootNode.value === value) {                          
-      oldNode=rootNode;                            
+    } else if (rootNode.value === value) {                
+      oldNode = rootNode;                            
       return (rootNode, value, oldNode);
-    }                               
-    else if (rootNode.value < value) {
-      oldNode=rootNode;                                
-      return this._findForRemove(rootNode.right, value, oldNode);                                  
+    } else if (rootNode.value < value) {
+      oldNode = rootNode;                                
+      return this._findForRemove(rootNode.right, value, oldNode);
     }
-    oldNode=rootNode;                                    
+    this.oldNode = rootNode;                                    
     return this._findForRemove(rootNode.left, value, oldNode);
   }
   
-  removeValueComparer(comparingNode,valueForComparison){
-    if(Math.abs(comparingNode.value-valueForComparison)<currentDifference)
-    {nodeToMove=comparingNode;
-    currentDifference=Math.abs(comparingNode.value-valueForComparison)}
-    if(comparingNode.left)
-    {this.removeValueComparer(comparingNode.left)}
-    if(comparingNode.right)
-    {this.removeValueComparer(comparingNode.right)}
+  removeValueComparer(comparingNode, valueForComparison) {
+    if (Math.abs(comparingNode.value - valueForComparison) < this.currentDifference) {
+      this.nodeToMove = comparingNode;
+      this.currentDifference = Math.abs(comparingNode.value - valueForComparison); 
+    }
+    if (comparingNode.left) {
+      this.removeValueComparer(comparingNode.left); 
+    }
+    if (comparingNode.right) {
+      this.removeValueComparer(comparingNode.right);
+    }
   }
-
 
 
   insert(nodeToInsert) {
@@ -105,7 +119,7 @@ class BinarySearchTree {
     return this._find(this.root, value);
   }
 
-  _find(rootNode, value){
+  _find(rootNode, value) {
     if (!rootNode) {
       return null;
     } else if (rootNode.value === value) {
